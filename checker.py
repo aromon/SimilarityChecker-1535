8663c299c8b6e9083ddff15f453b970340e4c5cd
Ho <ho@CRA.kr>
2024-06-13 16:12:54 +0900

    add check_alpha_function

--- checker.py
+++ checker.py
@@ -1,5 +1,5 @@
 class checkcheck:
-    def __init__(self,first_word,second_word):
+    def __init__(self, first_word, second_word):
         self.first_word = first_word
         self.second_word = second_word
 
@@ -8,7 +8,23 @@ class checkcheck:
             self.first_word, self.second_word = self.second_word, self.first_word
         first_len = len(self.first_word)
         second_len = len(self.second_word)
-        print(f'{self.first_word},{self.second_word} : {first_len},{second_len}')
         score = (1 - ((first_len - second_len) / second_len)) * 60
         return score
 
+    def alpha_check(self):
+        first_list = []
+        second_list = []
+        score = 0
+        for i in self.first_word:
+            if i not in first_list:
+                first_list.append(i)
+        for i in self.second_word:
+            if i not in second_list:
+                second_list.append(i)
+        total_list = second_list.copy()
+        for i in first_list:
+            if i in second_list:
+                score += 1
+            if i not in second_list:
+                total_list.append(i)
+        return (score / len(total_list)) * 40
--- test_main.py
+++ test_main.py
@@ -5,21 +5,29 @@ from checker import checkcheck
 
 class TestMain(TestCase):
     def test_first_len_check(self):
-        checked = checkcheck("ASD","DSA")
+        checked = checkcheck("ASD", "DSA")
         score = checked.len_check()
+        score2 = checked.alpha_check()
         self.assertEqual(60, int(score))
+        self.assertEqual(40, int(score2))
 
     def test_second_len_check(self):
         checked = checkcheck("A", "BB")
         score = checked.len_check()
+        score2 = checked.alpha_check()
         self.assertEqual(0, int(score))
+        self.assertEqual(0, int(score2))
 
     def test_third_len_check(self):
         checked = checkcheck("AAABB", "BAA")
         score = checked.len_check()
+        score2 = checked.alpha_check()
         self.assertEqual(20, int(score))
+        self.assertEqual(40, int(score2))
 
     def test_fourth_len_check(self):
         checked = checkcheck("AA", "AAE")
         score = checked.len_check()
+        score2 = checked.alpha_check()
         self.assertEqual(30, int(score))
+        self.assertEqual(20, int(score2))
